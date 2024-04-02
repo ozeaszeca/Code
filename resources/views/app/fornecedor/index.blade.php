f22969674aff05fdbf339d20016c48cccb7346a0
Ozeas Montenegro <ozeasmslima@gmail.com>
2024-04-01 22:23:50 -0400

    Incluindo Comentários, blocos e PHP puro

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,16 @@
 <h3>Fornecedor</h3>
+
+{{ 'Texto de teste'  }}
+<br>
+<?= 'Texto de teste' ?>
+<br>
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+    // Para comentário de uma linha
+    /*
+    * Para comentário de multiplas linhas
+    * */
+
+    echo 'Texto de teste';
+@endphp
